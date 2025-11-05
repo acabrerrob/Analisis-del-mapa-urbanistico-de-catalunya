88a76caca9a28243fa00df8ee91ea5e19cf15dae
acabrerrob <acabrerrob@educacion.navarra.es>
2025-11-05 14:43:02 +0100

    Javadoc del metodo 6 terminado

--- src/dades/LlistaDadesUrb.java
+++ src/dades/LlistaDadesUrb.java
@@ -180,8 +180,8 @@ public class LlistaDadesUrb {
      * superan el umbral de densidad de población por km 
      * cuadrado indicado por parámetro.
      * 
-     * @param vMinDensitat
-     * @return
+     * @param vMinDensitat  valor mínimo de densidad de población.
+     * @return  Lista de municipios con mayor densidad de población.
      */
     public String[] municipiMesDensos(double vMinDensitat){
         //Variable para medida total del array del return.
@@ -190,37 +190,39 @@ public class LlistaDadesUrb {
         String[] listaTemporal = new String[nDades];
 
         double densidadPob;
-        //Recorro la lista de la clase en busca de la cantidad de municipios que cumplen con las características para entrar en el array resultado.
+        
         for(int i = 0 ; i < listaDadesUrb.length ; i++){
             
             //Fórmula de dennsidad
             densidadPob = listaDadesUrb[i].getNumHabitants() * 100 / listaDadesUrb[i].getSuperficie_ha();
             
             //Solo entra si cumple el requisito del valor mínimo de densidad
-            if((densidadPob > vMinDensitat){
+            if(densidadPob > vMinDensitat){
 
                 //Variable para comprobar repeticiones
                 boolean repetido = false;
                 int j = 0;
                 //Comprobar que NO está en el array
-                while(!repetido && j < listaTemporal.length){
+                while(!repetido && j < mida){
 
                     //Condición para encontrar repetido
-                    if(listaTemporal[j].equals(listaDadesUrb[i].getNomMunicipi())){
+                    if(listaTemporal[j].equalsIgnoreCase(listaDadesUrb[i].getNomMunicipi())){
                         repetido = true;
-                    }else{
-                        mida += 1;
-                        j++;
                     }
+                    j++;
                 }
                 if(!repetido){
-                    listaTemporal[mida - 1] = listaDadesUrb[i].getNomMunicipi();
+                    listaTemporal[mida] = listaDadesUrb[i].getNomMunicipi();
+                    mida += 1;
                 }
             }
         }
-
+        //Creamos un array nuevo para que la lista quede mejor definida
         String[] listaMesDensos = new String [mida];
         
+        for(int i = 0 ; i < mida ; i++){
+            listaMesDensos[i] = listaTemporal[i];
+        }
         return listaMesDensos;
     }
 
